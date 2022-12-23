020d310756b3dec258871690f4e0883a15c489da
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-12-23 06:52:32 +0000

    条件分岐

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let word = "good";
-let word2 = "morning"
-let number = 2;
-let index =3;
-alert(word + word2);
-alert(number + index);
+let orange = 70;
+let apple = 100;
+if (orange < apple) {
+  alert("オレンジよりりんごの方が高いです");
+} else if (orange == apple) {
+  alert("オレンジとりんごは同じ値段です");
+} else {
+  alert("りんごよりオレンジの方が高いです");
+};
