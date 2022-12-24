f2a1f5dbe03f98a762e3182e653d046e683630e3
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-12-24 07:07:42 +0000

    var,let,constの使い方

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+// 再代入ができる
 var apple =100;
 alert(apple);
 var apple =110;
 alert(apple);
+
+let orange =120;
+console.log(orange);
+ orange =120;
+console.log(orange);
+
+// 再代入ができない
+// const grape =120;
+// console.log(grape);
+// grape =120;
+// console.log(grape);
