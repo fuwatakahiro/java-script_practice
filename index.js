77e707db4776a93d6a31a7f273f419e28cedfd88
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-12-24 06:51:57 +0000

    forぶん

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-let max=100;
-let number=1;
-let count =0;
-while(number < max){
-  number*=2;
-  count ++;
+let number = 0
+for(i =1;i<11;i++) {
+ number += i
 };
-alert("２をかけ続けて"+ max + "を超えるのに必要な回数は"  + count + "回です");
+alert("一から十まで足した合計は"+ number + "です");
